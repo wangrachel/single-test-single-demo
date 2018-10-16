6acbab0b3c8ed20c145ee94ef3e3c926b34f9e92
wangrachel <1141706486@qq.com>
2018-10-16 19:25:06 +0800

    新增测试：创建第二个分支

--- src/main/java/TestDemo.java
+++ src/main/java/TestDemo.java
@@ -1,5 +1,6 @@
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("This is the first Demo");
+        System.out.println("test create second branch");
     }
 }
